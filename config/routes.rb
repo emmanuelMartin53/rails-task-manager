0f82dd31b3c9c02ecf82f14a7abfe6e8e03da065
Emmanuel Martin <em.martin456@gmail.com>
2025-05-20 14:50:06 +0200

    POST and PATCH method DONE

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   get "tasks", to: "tasks#index", as: :tasks
+  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
   # root "posts#index"
 end
