998f03081c02fd47d8ac3eec6e9d5611f3be91cf
Emmanuel Martin <em.martin456@gmail.com>
2025-05-20 16:26:32 +0200

    Refactorisation task

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-
-  get "tasks", to: "tasks#index", as: :tasks
-  post "tasks", to: "tasks#create"
-  get "tasks/new", to: "tasks#new", as: :new_task
-  get "tasks/:id", to: "tasks#show", as: :task
+  resources :tasks
+  #get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id/edit", to: "tasks#edit", as: :task_edit
-  patch "tasks/:id", to: "tasks#update"
-  delete "tasks/:id", to: "tasks#destroy"
+
+  # get "tasks", to: "tasks#index", as: :tasks
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
   # root "posts#index"
 end
